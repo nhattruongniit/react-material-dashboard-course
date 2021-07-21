075ae4c772b805fd03572e4b919d62e47861dea1
TonyNguyen <truong.nn@amanotes.com>
2021-07-21 12:06:15 +0700

    add redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import clsx from 'clsx';
